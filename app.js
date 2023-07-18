159eb00b56313aaf688c731dc391a17d95c29670
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-19 00:00:09 +0530

    made routers

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const products_routes = require('./routes/product')
 
 app.get("/", (req, res) => {
     res.send("Hi aa");
 
 })
+app.use("/api/products", products_routes)
 
 const start = async () => {
     try {
