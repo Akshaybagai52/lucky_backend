7b28ed7561facec6d695661f4e1159d493bd1f1c
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-19 21:30:20 +0530

    get request

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,7 @@
-const getAllProducts = (req, res) => {
-    res.status(200).json({ msg: "I am getAllProducts"})
+const Product = require('../models/product')
+const getAllProducts = async (req, res) => {
+    const myData = await Product.find({})
+    res.status(200).json({ myData})
 }
 const getAllProductsTesting = (req, res) => {
     res.status(200).json({ msg: "I am testing"})
